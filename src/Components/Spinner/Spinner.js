949d125e28bdff63d0807a96f81e78b3bbf8c133
vedantPersi <vedant_khandokar@persistent.com>
2023-03-31 13:58:20 +0530

    Spinner Added

--- src/Components/Spinner/Spinner.js
+++ src/Components/Spinner/Spinner.js
@@ -1,15 +1,17 @@
 import React from "react";
+import BeatLoader from "react-spinners/BeatLoader";
 // import { usePromiseTracker } from "react-promise-tracker";
 // import { Loader } from "react-loader-spinner";
-// import "./spinner.css";
+//import "./spinner.css";
 
 export const Spinner = (props) => {
   //   const { promiseInProgress } = usePromiseTracker();
 
   return (
-    <div className="spinner">
+    <div>
       {/* <Loader type="ThreeDots" color="#2BAD60" height="100" width="100" /> */}
-      <p>Loading ..</p>
+      {/* <p>Loading ..</p> */}
+      <BeatLoader color="#22c55e" size={20}/>
     </div>
   );
 };
